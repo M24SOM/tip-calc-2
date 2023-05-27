e1cef40365fa9c2dcc1f177241b779d0b909c499
Mohamed Mahad Farah <mmfarah@proton.me>
2023-05-27 12:24:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello");
+function calcTip(bill) {
+  let tip;
+  if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15;
+  } else {
+    tip = bill * 0.2;
+  }
+  let total = bill + tip;
+  console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+  return tip;
+}
+
+calcTip(275); // Output: The bill was 275, the tip was 41.25, and the total value 316.25
